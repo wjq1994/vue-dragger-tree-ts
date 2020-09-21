7a137f97ddc0cb6fc6bc992afc8e9af3b87c590c
wangjianqing <wangjianqing@ktjr.com>
2020-09-21 17:35:43 +0800

    修改测试页面

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-    <tree :allowDrop="allowDrop" nodeKey="id" ref="tree" :list="data" @node-click="nodeClick">
+    <tree nodeKey="id" ref="tree" :list="data" @node-click="nodeClick">
         <span class="custom-tree-node" slot-scope="{ node, data }">
             <span>{{ data.label }}</span>
             <span>
@@ -15,24 +15,9 @@
     </tree>
     <br />
     <br />
-    <tree nodeKey="id" ref="tree1" :list="data1">
+    <tree :dragOptions="dragOptionsRight" nodeKey="id" ref="tree1" :list="data1">
         <span class="custom-tree-node" slot-scope="{ node, data }">
             <span>{{ data.label }}</span>
-            <span>
-                <el-button type="text" size="mini" @click.stop="() => append(node, data)">
-                    Append
-                </el-button>
-                <el-button type="text" size="mini" @click.stop="() => remove(node, data)">
-                    Delete
-                </el-button>
-            </span>
-        </span>
-    </tree>
-    <br />
-    <br />
-    <tree nodeKey="id" ref="tree2" :list="rules">
-        <span class="custom-tree-node" slot-scope="{ node, data }">
-            <span>{{ data.name }}</span>
             <span>
                 <el-button type="text" size="mini" @click.stop="() => append(node, data)">
                     Append
@@ -65,10 +50,16 @@ import {
     }
 })
 export default class Index extends BaseVue {
-    public allowDrop: boolean = true;
-    public defaultProps: any = {
-        children: "children",
-        label: "label",
+    /** 拖拽参数 右 */
+    public dragOptionsRight: any = {
+        animation: 0,
+        group: {
+            name: "description",
+            pull: "clone",
+            put: false
+        },
+        disabled: false,
+        ghostClass: "ghost"
     };
     public data: any = [{
             label: "一级 1",
@@ -134,16 +125,6 @@ export default class Index extends BaseVue {
             }, ],
         }, ],
     }, ];
-    public data0: any = [{
-        label: "一级 1",
-        children: [],
-    }, ];
-    public rules: any[] = [];
-    public onClickTest() {
-        console.log("this.data", this.data);
-        console.log("this.treeManage", (this.$refs.tree as any).treeManage);
-        console.log("this.treeManage1", (this.$refs.tree1 as any).treeManage);
-    }
     // 增
     public append(node, data) {
         console.log("append")
@@ -165,106 +146,7 @@ export default class Index extends BaseVue {
     }
 
     public onCreated() {
-        let data = [{
-                "name": "目录01", // 文件或目录名称
-                "type": 2, // 映射规则节点类型, 1: 文件, 2: 目录
-                "required": true, // 是否必备, 此字段对目录类型的节点无意义，可不填
-                "mappingType": 1, //  映射类型，1: 普通映射, 2: 合并映射, 此字段对目录类型的节点无意义，可不填
-                "children": [ // 子节点, 此字段对文件类型的节点无意义，可不填
-                    {
-                        "name": "XX合同", // 映射后的文件名，占位符格式: {{合同号}}, {{原告}}, {{被告}}, 合并映射不支持占位符
-                        "type": 1, // 文件
-                        "required": true, // 是否必备
-                        "mappingType": 1, //  映射类型，1: 普通映射, 2: 合并映射
-                        "sources": [ // 对于 mappingType 为 1 的节点来说此数组型字段有且只有一个元素
-                            {
-                                "originalName": "合同ABC",
-                                "required": true, // 是否必备, 对于 mappingType 为 1 的节点来说此字段的值无所谓
-                                "pageNoRequired": false // 是否需要页码，对于 mappingType 为 1 的节点来说此字段的值无所谓
-                            }
-                        ]
-                    },
-                    {
-                        "name": "XX合同",
-                        "type": 1, // 文件
-                        "required": true, // 是否必备
-                        "mappingType": 1, //  映射类型，1: 普通映射, 2: 合并映射
-                        "sources": [ // 对于 mappingType 为 1 的节点来说此数组型字段有且只有一个元素
-                            {
-                                "originalName": "合同ABC",
-                                "required": true, // 是否必备, 对于 mappingType 为 1 的节点来说此字段的值无所谓
-                                "pageNoRequired": false // 对于 mappingType 为 1 的节点来说此字段的值无所谓
-                            }
-                        ]
-                    },
-                    {
-                        "name": "XX合同",
-                        "type": 1, // 文件
-                        "required": true,
-                        "mappingType": 2,
-                        "sources": [{
-                                "originalName": "合同1",
-                                "pageNoRequired": false
-                            },
-                            {
-                                "originalName": "合同2",
-                                "required": true,
-                                "pageNoRequired": true
-                            },
-                            {
-                                "originalName": "合同1",
-                                "required": true,
-                                "pageNoRequired": true
-                            }
-                        ]
-                    },
-                ]
-            },
-            {
-                "name": "目录02",
-                "type": 2, // 目录
-                "children": [{
-                    "name": "XX合同",
-                    "type": 1, // 文件
-                    "required": true,
-                    "mappingType": 2,
-                    "sources": [{
-                            "originalName": "合同1",
-                            "pageNoRequired": false
-                        },
-                        {
-                            "originalName": "合同2",
-                            "required": true,
-                            "pageNoRequired": true
-                        },
-                        {
-                            "originalName": "合同1",
-                            "required": true,
-                            "pageNoRequired": true
-                        }
-                    ]
-                }, ]
-            }
 
-        ];
-
-        function getData(data) {
-            data.forEach((item) => {
-                if (item.type === 2) {
-                    item.iconClass = 'el-icon-folder';
-                } else {
-                    item.iconClass = 'el-icon-document';
-                }
-
-                if (item.children && item.children.length > 0) {
-                    getData(item.children);
-                }
-            })
-        }
-
-        getData(data);
-
-        this.rules = data;
     }
 
 }
